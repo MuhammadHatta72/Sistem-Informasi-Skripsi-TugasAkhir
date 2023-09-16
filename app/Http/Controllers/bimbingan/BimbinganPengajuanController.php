9d8e020e8b136316210347a6aaff3ebb725f19b1
BrianMohamadSafiudin <brianms2004@gmail.com>
2023-09-16 11:57:28 +0700

    fix: form pengajuan bimbingan

--- app/Http/Controllers/bimbingan/BimbinganPengajuanController.php
+++ app/Http/Controllers/bimbingan/BimbinganPengajuanController.php
@@ -17,30 +17,27 @@ class BimbinganPengajuanController extends Controller
 
     public function create(Request $request)
     {
-        return view('mahasiswa.bimbingan_pengajuan');
+        $data = [
+            'status_proposal' => true
+        ];
+    return view('mahasiswa.bimbingan_pengajuan', $data);
     }
 
     public function store(Request $request)
     {
         $request->validate([
             'judul' => 'required|string|max:255',
-            'data1' => 'required|string',
-            'data2' => 'required|string',
-            'data3' => 'required|string',
+            'proposalbimbingan' => 'required|mimes:pdf|max:20000',
         ], [
             'judul.required' => 'Judul bimbingan wajib diisi.',
-            'data1.required' => 'Data 1 wajib diisi.',
-            'data2.required' => 'Data 2 wajib diisi.',
-            'data3.required' => 'Data 3 wajib diisi.',
+            'proposalbimbingan' => 'Proposal bimbingan wajib diisi.',
         ]);
 
         $bimbingan = new Bimbingan();
         $bimbingan->id_mahasiswa = Auth::user()->mahasiswa->id;
         $bimbingan->judul = $request->input('judul');
-        $bimbingan->data1 = $request->input('data1');
-        $bimbingan->data2 = $request->input('data2');
-        $bimbingan->data3 = $request->input('data3');
-
+        $bimbingan->status = 'dikirim';
+        $bimbingan->path_dokumen = $request->input('path_dokumen');
         $bimbingan->save();
 
         return redirect()->route('bimbingan_pengajuan.index')->with('success', 'Bimbingan berhasil diajukan');
